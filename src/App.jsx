0b682cb26a5ff4a944ef883ffaf2bb1298b99a01
Enma Ormaza <enma.ormaza@mataro.epiaedu.cat>
2024-02-02 20:38:05 +0100

    FINISH CHALLENGE REACT 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-  const name = "Enma";
+import {Total} from './Total.jsx';
+import {Header} from './Header.jsx';
+import {Content} from './Content.jsx';  
+const App = () => {
+  const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  // const part3 = 'State of a component'
+  const parts = [
+    {name: 'Fundamentals of React', exercises: 10},
+    {name: 'Using props to pass data', exercises: 7},
+    {name: 'State of a component', exercises: 14}
+  ]
+  const exercises3 = 14
 
   return (
-    <>
-      <h1>{`Hello ${name}`}</h1>
-    </>
+    <div>
+      <Header course={course} />
+      <Content parts={parts}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+    </div>
   )
 }
 
